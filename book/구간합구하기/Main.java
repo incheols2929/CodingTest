565ebe787b88d2a7112cbec285808588066bbde9
incheols <incheols2929@gmail.com>
2025-05-24 16:39:31 +0900

    25.05.24 공부 내용

--- book/구간합구하기/Main.java
+++ book/구간합구하기/Main.java
@@ -1,12 +1,33 @@
 package book.구간합구하기;
-/* 1. 최악의 상황을 대비해야하는 것을 풀려고 해야함.
- *
- *
+/* 1. 최악의 상황을 케이스를 생각해야함.
+ * 2. 배열의 길이가 100,000개를 생각해야함. 질의 횟수도. 1 ~ 10만개
  * */
 
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
+
+        int suNo = Integer.parseInt(stringTokenizer.nextToken());
+        int quizNo = Integer.parseInt(stringTokenizer.nextToken());
+
+        long[] S = new long[suNo + 1]; // 0번째 index를 무시하고자 +1을 넣음
+        stringTokenizer = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= suNo; i++) {
+            S[i] = S[i - 1] + Integer.parseInt(stringTokenizer.nextToken());
+        }
+        for (int q = 0; q < quizNo; q++) {
+            stringTokenizer = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(stringTokenizer.nextToken());
+            int j = Integer.parseInt(stringTokenizer.nextToken());
+            System.out.println(S[j] - S[i - 1]);
+        }
 
     }
 }
